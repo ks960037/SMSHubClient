b9d2a5b82b3ef51f5a4328b43bca4c886615ec51
Tumurin <kuanminzhao@gmail.com>
2023-12-29 03:08:19 +0800

    後臺管理首頁

--- pages/backstage/accountTopup.vue
+++ pages/backstage/accountTopup.vue
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="mx-auto col-12">
                 <div class="mt-4 card card-body">
-                    <h1>儲值</h1>
+                    <h1>帳號儲值</h1>
                     帳號清單
                     加值金額
                     確定加值
--- pages/backstage/index.vue
+++ pages/backstage/index.vue
@@ -6,13 +6,87 @@
                     <h1>
                         後臺管理
                     </h1>
-                    功能卡片
+                    <div class="row">
+                        <div v-for="{
+                            title,
+                            value,
+                            description,
+                            icon: { component, background, shape },
+                            link,
+                        } of cardData" :key="title" class="col-lg-3 col-md-6 col-12">
+                            <MiniStatisticsCard :title="title" :value="value" :description="description" :link="link" :icon="{
+                                component: component,
+                                background: background,
+                                shape: shape,
+                            }" />
+                        </div>
+                    </div>
+                    <div class="row">
+                        <GradientLineChart :chart-options="{
+                            chart: {
+                                type: 'area',
+                            },
+                            colors: ['#4BB543'],
+                            labels: [
+                                'Apr',
+                                'May',
+                                'Jun',
+                                'Jul',
+                                'Aug',
+                                'Sep',
+                                'Oct',
+                                'Nov',
+                                'Dec',
+                            ],
+                            dataLabels: {
+                                enabled: false,
+                            },
+                            stroke: {
+                                curve: 'smooth',
+                            },
+                            legend: {
+                                show: false,
+                            },
+                        }" :series="[
+    {
+        name: 'Mobile Apps',
+        data: [50, 40, 300, 220, 500, 250, 400, 230, 500],
+    },
+]" />
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </template>
 
+<script setup>
+import GradientLineChart from "~~/examples/charts/GradientLineChart.vue";
+const cardData = [
+    {
+        title: "",
+        value: "創建帳號",
+        description: `創建新帳號`,
+        icon: {
+            component: "ni ni-zoom-split-in",
+            background: "bg-gradient-info",
+            shape: "rounded-circle",
+        },
+        link: "/backstage/accountCreate",
+    },
+    {
+        title: "",
+        value: "帳號儲值",
+        description: `為已建立的帳號進行儲值`,
+        icon: {
+            component: "ni ni-money-coins",
+            background: "bg-gradient-success",
+            shape: "rounded-circle",
+        },
+        link: "/backstage/accountTopup",
+    },
+];
+</script>
 <script>
 definePageMeta({
     layout: "backstage",
