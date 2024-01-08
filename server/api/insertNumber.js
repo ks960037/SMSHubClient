7fb50122277cdd73367f95fd0ea726a60007a443
ks960037 <ks960037@gmail.com>
2024-01-09 00:02:01 +0800

    建立 Model & 插入資料API OK

--- server/api/insertNumber.js
+++ server/api/insertNumber.js
@@ -1,41 +1,22 @@
-// import mongoose from "mongoose";
-// import { getSchema } from "../models/Number.js"
+import dbconnection from "../plugins/dbconnection.js";
+import PHNumber from "../models/PHNumber.js";
 
-// // numbers db
-// // const connUri = process.env.MONGO_NUMBERS_CONN_URL;
-// const connUri = process.env.MONGO_LOCAL_CONN_URL;
-// const conn = mongoose.createConnection(connUri, {
-//     ssl: true,
-//     // For example, see https://medium.com/@rajanmaharjan/secure-your-mongodb-connections-ssl-tls-92e2addb3c89
-//     // for where the `rootCA.pem` file comes from.
-//     // Please note that, in Mongoose >= 5.8.3, `sslCA` needs to be
-//     // the **path to** the CA file, **not** the contents of the CA file
-//     tlsCAFile: `./mongoKey/ca.pem`,
-//     tlsCertificateKeyFile: `./mongoKey/client.pem`,
-//     tlsAllowInvalidHostnames: true,
-//     tlsAllowInvalidCertificates: false,
-//     serverSelectionTimeoutMS: 60000,
-// });
+export default defineEventHandler(async (event) => {
+    console.log("插入資料");
+    // 連線
+    dbconnection();
 
-// conn.once("open", () =>
-//     console.log(
-//         "MongoDB --  numbers database connection established 成功!"
-//     )
-// );
-// conn.on("error", (err) => {
-//     console.log(
-//         "MongoDB connection error. Please make sure MongoDB is running. " + err
-//     );
-//     process.exit();
-// });
+    let n = new PHNumber({
+        _id: null,
+        phone_number: "0012345678",
+        update_at: new Date(),
+        exported_at: new Date()
+    });
 
-// function getModel(conn, collectionName) {
-//     return conn.model(collectionName, getSchema(collectionName));
-// }
-
-// export default {
-//     IDN: getModel(conn, "62_indonesia"),
-//     IND: getModel(conn, "91_india"),
-//     PHL: getModel(conn, "63_philippines"),
-//     THA: getModel(conn, "66_TH"),
-// };
+    try {
+        let saved = await n.save();
+        console.log(saved.phone_number);
+    } catch (err) {
+        console.log(err);
+    }
+})
